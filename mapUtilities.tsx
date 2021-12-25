2cd26fb2b4d365afb8af32da889c3d9aad558e3f
Markus Schulze <markus.schulze@physik.hu-berlin.de>
2021-12-25 23:55:53 +0100

    changed collides

--- mapUtilities.tsx
+++ mapUtilities.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="mapUtilities" tilewidth="32" tileheight="32" tilecount="9" columns="9">
  <image source="../world-map-starterkit/tiles_png/structures/mapUtilities.png" width="288" height="32"/>
+ <tile id="3">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
