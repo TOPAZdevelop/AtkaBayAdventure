d4d28958bdc9e791df9bbf1ee1c963f170538775
Markus Schulze <markus.schulze@physik.hu-berlin.de>
2021-12-25 23:16:11 +0100

    added tsx

---
+++ blocker.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="blocker" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <properties>
+  <property name="collides" type="bool" value="true"/>
+ </properties>
+ <image source="blocker.png" width="32" height="32"/>
+</tileset>
