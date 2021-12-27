0d43ce53ac8d20bb0b7e8fa2c45e1b825f2b09f8
Markus Schulze <markus.schulze@physik.hu-berlin.de>
2021-12-27 14:32:02 +0100

    test copyright property

--- rc3-mullana-overlay.tsx
+++ rc3-mullana-overlay.tsx
@@ -1,4 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="rc3-mullana-overlay" tilewidth="32" tileheight="32" tilecount="512" columns="16">
+ <properties>
+  <property name="tilesetCopyright" value="none"/>
+ </properties>
  <image source="../world-tiles/world-tiles/rc3-2020/structures/rc3-mullana-overlay.png" width="512" height="1024"/>
 </tileset>
